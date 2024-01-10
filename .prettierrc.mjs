0620b08207b43b6801ffa8fa162004e91e197fe2
PaulHaze <dev@artemist.com.au>
2024-01-10 20:53:54 +1100

    fix double quote issue

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,20 +1,19 @@
 /** @type {import("prettier").Config} */
 export default {
-  ...require("prettier-config-standard"),
-  printWidth: 100,
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: "es5",
-  useTabs: true,
-  pluginSearchDirs: [__dirname],
-  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-  ],
+	printWidth: 100,
+	semi: true,
+	singleQuote: true,
+	tabWidth: 2,
+	trailingComma: 'es5',
+	useTabs: true,
+	// pluginSearchDirs: [__dirname],
+	plugins: ['prettier-plugin-astro', 'prettier-plugin-tailwindcss'],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
 };
