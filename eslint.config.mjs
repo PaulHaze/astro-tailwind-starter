8b8cfdb789389ff0aea1862f895b384e4a456109
PaulHaze <dev@artemist.com.au>
2024-05-09 08:21:41 +1000

    general clean up

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
+import js from '@eslint/js';
 
 export default [
 	// add more generic rule sets here, such as:
 	// js.configs.recommended,
+	js.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
 	{
 		rules: {
 			// override/add rules settings here, such as:
 			// "astro/no-set-html-directive": "error"
-			'no-unused-vars': 'warn',
+			// 'no-unused-vars': 'warn',
 		},
 	},
 ];
