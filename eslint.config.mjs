0d0624a1e0d2813b6a55a74f5fd42276ef34a005
PaulHaze <dev@artemist.com.au>
2024-05-15 08:09:16 +1000

    update to latest typescript-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
 import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default [
 	// add more generic rule sets here, such as:
 	// js.configs.recommended,
 	js.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
+	...tseslint.configs.recommended,
 	{
 		rules: {
 			// override/add rules settings here, such as:
