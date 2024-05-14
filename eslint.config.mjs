2e2fe7f4c69678f1a21f1a426f9a973a8673f9f8
PaulHaze <dev@artemist.com.au>
2024-05-15 09:31:41 +1000

    add prettier plug in for eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,21 @@
 import eslintPluginAstro from 'eslint-plugin-astro';
-import js from '@eslint/js';
+import eslintjs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 export default [
 	// add more generic rule sets here, such as:
 	// js.configs.recommended,
-	js.configs.recommended,
-	...eslintPluginAstro.configs.recommended,
+	eslintjs.configs.recommended,
 	...tseslint.configs.recommended,
+	...eslintPluginAstro.configs.recommended,
 	{
 		rules: {
 			// override/add rules settings here, such as:
 			// "astro/no-set-html-directive": "error"
 			// 'no-unused-vars': 'warn',
+			'no-unused-vars': 'warn',
 		},
 	},
+	eslintPluginPrettierRecommended,
 ];
