be4d02df96ca4c019555dbd0cb72209d72146edf
PaulHaze <dev@artemist.com.au>
2025-02-26 12:07:41 +1100

    update packages and eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,28 @@
-// import eslintjs from '@eslint/js';
+import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
-export default [
-	// add more generic rule sets here, such as:
-	// js.configs.recommended,
-	// eslintjs.configs.recommended,
-	...tseslint.configs.recommended,
-	...tseslint.configs.stylistic,
+export default tseslint.config(
+	eslint.configs.recommended,
+	tseslint.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
 	{
-		// TS specific rules
+		// our own additional config
 		languageOptions: {
 			parser: tseslint.parser,
 		},
-		files: ['*.ts', '*.tsx'],
+		files: ['src/**/*.ts', 'src/**/*.tsx'],
 		plugins: {
 			// additional plugins
 		},
-		rules: {},
+		rules: {
+			'@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+		},
 	},
 	{
 		// override/add rules settings here, such as:
 		// "astro/no-set-html-directive": "error"
 		// 'no-unused-vars': 'warn'
-	},
-];
+		rules: {},
+	}
+);
