7c181e27deb1c497de874726167b712a1169f579
PaulHaze <dev@artemist.com.au>
2026-03-24 11:10:04 +1100

    udpated deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import eslintPluginAstro from 'eslint-plugin-astro';
 
-export default tseslint.config(
+export default tseslint.config([
 	eslint.configs.recommended,
 	tseslint.configs.recommended,
 	...eslintPluginAstro.configs.recommended,
@@ -25,4 +25,4 @@ export default tseslint.config(
 		// 'no-unused-vars': 'warn'
 		rules: {},
 	}
-);
+]);
